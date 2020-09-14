a44966231db97e48c128389787c551ae01f517d0
percy <lsnh@foxmail.com>
2020-09-14 13:59:02 +0800

    add vmrun

---
+++ shell_scripts/colab_daemon.py
@@ -0,0 +1,177 @@
+import time
+from datetime import datetime, timezone, timedelta
+from lxml import etree
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+import json
+
+
+def get_config():
+    """读取配置信息"""
+    with open("/tmp/colab_daemon.json", "r") as fp:
+        return json.load(fp)
+
+
+def get_driver():
+    """实例化webdriver"""
+    options = webdriver.ChromeOptions()
+    options.add_argument('--headless')
+    options.add_argument('--no-sandbox')
+    options.add_argument('--disable-dev-shm-usage')
+    options.add_argument('--incognito')
+    options.add_argument('--disable-notifications')
+    user_agent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.116 Safari/537.36"
+    options.add_argument("--user-agent={}".format(user_agent))
+    driver = webdriver.Chrome('chromedriver', options=options)
+    return driver
+
+
+is_running = True
+globel_driver = get_driver()
+
+
+def write_log(message):
+    """记录日志"""
+    timenow = datetime.utcnow().replace(tzinfo=timezone.utc).astimezone(
+        timezone(timedelta(hours=8)))
+    file_name = './log/colab_daemon{}.log'.format(timenow.strftime("%Y%m"))
+    with open(file_name, 'a+', encoding='utf-8') as f:
+        message = '{} {}'.format(timenow.strftime("%m-%d %H:%M:%S"), message)
+        print(message)
+        f.write(message + "\n")
+        message = "当前url: {}".format(globel_driver.current_url) + "\n"
+        print(message)
+        f.write(message)
+        f.close()
+
+
+def execute_code(driver):
+    """等待加载完成"""
+    try:
+        for i in range(1000):
+            tree = etree.HTML(driver.page_source)
+            if len(tree.xpath('//colab-run-button')) == 1:
+                print("加载完成")
+                break
+            else:
+                time.sleep(0.1)
+                continue
+        code_run_element = driver.execute_script(
+            '''return document.querySelector("div.main-content > div.codecell-input-output > div.inputarea.horizontal.layout.code > div.cell-gutter > div > colab-run-button").shadowRoot.querySelector("div > div.cell-execution-indicator");'''
+        )
+        code_run_element.click()
+    except Exception as e:
+        write_log("点击执行报错:{}".format(e))
+        driver.implicitly_wait(20)
+    time.sleep(3)
+
+
+def get_running_status(driver):
+    """获取最新状态"""
+    tree = etree.HTML(driver.page_source)
+    if len(tree.xpath('//colab-run-button')) == 0:
+        fresh_page(driver)
+        time.sleep(10)
+    return tree.xpath('//colab-run-button/@title')[0]
+
+
+def login(driver):
+    script_url = "https://www.google.com?hl=en"
+    driver.get(script_url)
+    driver = read_cookies(driver)
+    driver.get(get_config()["script_url"])
+    write_log("开始登录")
+    execute_code(driver)
+    if 'Interrupt execution' not in get_running_status(driver):
+        execute_code(driver)
+        time.sleep(3)
+        # code_input_element.send_keys(Keys.CONTROL, Keys.ENTER)
+    write_log('当前状态:' + get_running_status(driver))
+    run_deamon(driver)
+
+
+def fresh_page(driver):
+    try:
+        driver.refresh()
+        driver.switch_to_alert().accept()
+    except Exception as e:
+        write_log("刷新报错:{}".format(e))
+        driver.implicitly_wait(20)
+    return driver
+
+
+def save_cookie(driver):
+    """保存cookie"""
+    cookies = driver.get_cookies()
+    with open("/tmp/cookies.json", "w") as fp:
+        json.dump(cookies, fp)
+        fp.close()
+
+
+def read_cookies(driver):
+    """读取cookies"""
+    with open("/tmp/cookies.json", "r") as fp:
+        cookies = json.load(fp)
+        for cookie in cookies:
+            if 'expiry' in cookie:
+                del cookie['expiry']
+            if 'sameSite' in cookie:
+                del cookie['sameSite']
+            driver.add_cookie(cookie)
+    return driver
+
+
+def run_deamon(driver):
+    duration = 0
+    error_count = 0
+    write_log("开始进程守护")
+    while is_running:
+        for i in range(100):
+            try:
+                save_cookie(driver)
+                tree = etree.HTML(driver.page_source)
+                if len(tree.xpath('//colab-run-button/@title')) == 0:
+                    time.sleep(0.1)
+                    continue
+                statues_description = get_running_status(driver)
+                if len(tree.xpath("/html/body/iron-overlay-backdrop")) > 0:
+                    driver.find_element_by_xpath('//*[@id="ok"]').click()
+                    break
+                if duration > 3600 or error_count > 10:
+                    duration = 0
+                    error_count = 0
+                    driver = fresh_page(driver)
+                    execute_code(driver)
+                    continue
+                if 'Interrupt execution' not in statues_description:
+                    write_log('点击运行前' + statues_description)
+                    execute_code(driver)
+                    time.sleep(30)
+                    write_log('点击运行后:' + get_running_status(driver))
+                    time.sleep(30)
+                    if 'Interrupt execution' not in get_running_status(driver):
+                        fresh_page(driver)
+                elif 'Interrupt execution' in get_running_status(driver):
+                    time.sleep(1)
+                    duration += 1
+                if duration % 100 == 0:
+                    write_log("当前duration：{}, {}".format(
+                        duration, get_running_status(driver)))
+                continue
+            except Exception as e:
+                error_count += 1
+                write_log("error 守护进程:{}".format(e))
+                time.sleep(0.1)
+        if get_config()["script_url"] != driver.current_url:
+            break
+    if not is_running:
+        time.sleep(10)
+    write_log("重新登录")
+    globel_driver = driver
+
+
+if __name__ == "__main__":
+    login(globel_driver)
---
+++ shell_scripts/colab_init.sh
@@ -0,0 +1,162 @@
+#!/usr/bin/env bash
+ln -sf /usr/share/zoneinfo/Asia/Shanghai /etc/localtime
+
+if [ ! "$(command -v bash-completion)" ]; then
+  cat >>~/.bashrc <<-EOF
+if ! shopt -oq posix; then
+  if [ -f /usr/share/bash-completion/bash_completion ]; then
+    . /usr/share/bash-completion/bash_completion
+  elif [ -f /etc/bash_completion ]; then
+    . /etc/bash_completion
+  fi
+fi
+EOF
+fi
+
+echo "安装依赖..."
+sudo apt update -qq && sudo apt install -qq -y shadowsocks-libev rng-tools supervisor vim htop chromium-chromedriver git jq bash-completion ssh >/dev/null
+source ~/.bashrc
+
+echo "设置配置信息"
+init_config=$(cat /tmp/init.config | base64 -di --decode)
+instance_name=$(echo $init_config | jq -r '.instance_name')
+frp_version=$(echo $init_config | jq -r '.frp_version')
+frp_token=$(echo $init_config | jq -r '.frp_token')
+frp_server_domain=$(echo $init_config | jq -r '.frp_server_domain')
+frp_server_addr=$(echo $init_config | jq -r '.frp_server_addr')
+frp_server_port=$(echo $init_config | jq -r '.frp_server_port')
+ssh_port=$(echo $init_config | jq -r '.ssh_port')
+ssh_password=$(echo $init_config | jq -r '.ssh_password')
+shadowsocksport=$(echo $init_config | jq -r '.shadowsocksport')
+shadowsockspwd=$(echo $init_config | jq -r '.shadowsockspwd')
+shadowsockscipher=$(echo $init_config | jq -r '.shadowsockscipher')
+
+echo "安装frpc"
+wget -q https://github.com/fatedier/frp/releases/download/v${frp_version}/frp_${frp_version}_linux_amd64.tar.gz
+tar -xzf frp_${frp_version}_linux_amd64.tar.gz
+\cp -rf frp_${frp_version}_linux_amd64/frpc /usr/bin/frpc
+
+if [ ! -d /etc/frp ]; then
+  mkdir -p /etc/frp
+fi
+
+cat >/etc/frp/frpc.ini <<-EOF
+[common]
+server_addr = ${frp_server_addr}
+server_port = ${frp_server_port}
+token = ${frp_token}
+
+[colab.${instance_name}.ssh]
+type = tcp
+local_ip = localhost
+local_port = 22
+remote_port = ${ssh_port}
+custom_domains = ${frp_server_domain}
+
+[colab.${instance_name}.ss]
+type = tcp
+local_ip = localhost
+local_port = ${shadowsocksport}
+remote_port = ${shadowsocksport}
+use_encryption = true
+use_compression = true
+custom_domains = ${frp_server_domain}
+
+[colab.${instance_name}.ss.udp]
+type = udp
+local_ip = localhost
+local_port = ${shadowsocksport}
+remote_port = ${shadowsocksport}
+use_encryption = true
+use_compression = true
+custom_domains = ${frp_server_domain}
+EOF
+rm -rf frp_${frp_version}_linux_amd64* /tmp/init.
+
+echo "安装selenium"
+if [ ! -f /usr/lib/chromium-browser/chromedriver ]; then
+  \cp -rf /usr/lib/chromium-browser/chromedriver /usr/bin
+fi
+pip3 install -q selenium pyperclip apscheduler lxml >/dev/null
+if [ ! -d /opt/colab_daemon ]; then
+  echo "安装colab_daemon"
+  mkdir -p /opt/colab_daemon/log
+  wget -qO /opt/colab_daemon/app.py https://raw.githubusercontent.com/pengpercy/code_snippets/master/shell_scripts/colab_daemon.py
+fi
+
+echo "安装shadowsocks"
+cat >/etc/shadowsocks-libev/config.json <<-EOF
+{
+    "server":"0.0.0.0",
+    "server_port":${shadowsocksport},
+    "password":"${shadowsockspwd}",
+    "timeout":300,
+    "user":"nobody",
+    "method":"${shadowsockscipher}",
+    "fast_open":false,
+    "nameserver":"8.8.8.8",
+    "mode":"tcp_and_udp",
+    "plugin":"v2ray-plugin",
+    "plugin_opts":"server"
+}
+EOF
+if [ ! -f /usr/bin/v2ray-plugin ]; then
+  wget -qO v2ray-plugin.tar.gz https://github.com/shadowsocks/v2ray-plugin/releases/download/v1.2.0/v2ray-plugin-linux-amd64-v1.2.0.tar.gz
+  tar -xzf v2ray-plugin.tar.gz && mv v2ray-plugin_linux_amd64 /usr/bin/v2ray-plugin && rm v2ray-plugin.tar.gz
+fi
+
+echo "配置supervisor"
+cat >/etc/supervisor/conf.d/frpc.conf <<-EOF
+[program:frpc]
+command = frpc -c /etc/frp/frpc.ini
+directory = /etc/frp/
+autostart = true
+autorestart = true
+stdout_logfile = /var/log/frp.log
+stderr_logfile = /var/log/frp.err.log
+numprocs = 1
+startretries = 100
+stopsignal = KILL
+stopwaitsecs = 10
+EOF
+
+cat >/etc/supervisor/conf.d/colab_daemon.conf <<-EOF
+[program:colab_daemon]
+command = python3 /opt/colab_daemon/app.py
+directory = /opt/colab_daemon
+autostart = true
+autorestart = true
+stdout_logfile = /var/log/colab_daemon.log
+stderr_logfile = /var/log/colab_daemon.err.log
+numprocs = 1
+startretries = 100
+stopsignal = KILL
+stopwaitsecs = 10
+EOF
+
+cat >/etc/supervisor/conf.d/shadowsocks-libev.conf <<-EOF
+[program:ss-server]
+command = ss-server -c /etc/shadowsocks-libev/config.json
+directory = /etc/shadowsocks-libev/
+autostart = true
+autorestart = true
+stdout_logfile = /var/log/shadowsocks.log
+stderr_logfile = /var/log/shadowsocks.err.log
+numprocs = 1
+startretries = 100
+stopsignal = KILL
+stopwaitsecs = 10
+EOF
+
+if [ ! -f /var/log/frp.log ]; then
+  sudo service supervisor start
+fi
+
+echo "安装ssh"
+sed -re 's/^(\#)(Port)([[:space:]]+)(.*)/\2\3\4/' /etc/ssh/sshd_config >~/temp.cnf && mv -f ~/temp.cnf /etc/ssh/sshd_config
+sed -re 's/^(\#)(ListenAddress)([[:space:]]+)(0\.0\.0\.0)(.*)/\2\3\4/' /etc/ssh/sshd_config >~/temp.cnf && mv -f ~/temp.cnf /etc/ssh/sshd_config
+sed -re 's/^(\#)(PermitRootLogin)([[:space:]]+)(prohibit-password)(.*)/\2\3\4/' /etc/ssh/sshd_config >~/temp.cnf && mv -f ~/temp.cnf /etc/ssh/sshd_config
+sed -re 's/^(PermitRootLogin)([[:space:]]+)prohibit-password/\1\2yes/' /etc/ssh/sshd_config >~/temp.cnf && mv -f ~/temp.cnf /etc/ssh/sshd_config && (echo "${ssh_password}" && echo "${ssh_password}") | sudo passwd root
+service ssh restart
+
+touch /var/log/frp.log
